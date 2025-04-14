26f03ea8af778f59687353519be58cd2b21d475f
pex-dev <contacto@brayandev.com>
2025-04-14 14:34:56 -0400

    Se tradujieron los emails de verificación y reestablecimiento de contraseña

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +23,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        //Email personalizado para verificación de email
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->greeting(Lang::get('notifications.greeting', ['name' => $notifiable->name]))
+                ->subject(Lang::get('notifications.verify_email.subject'))
+                ->line(Lang::get('notifications.verify_email.intro'))
+                ->action(Lang::get('notifications.verify_email.action'), $url)
+                ->line(Lang::get('notifications.verify_email.outro'))
+                ->salutation(Lang::get('notifications.salutation'));
+        });
+
+        //Email personalizado para reestablecer contraseña
+        ResetPassword::toMailUsing(function (object $notifiable, string $url) {
+            return (new \Illuminate\Notifications\Messages\MailMessage)
+                ->greeting(Lang::get('notifications.greeting', ['name' => $notifiable->name]))
+                ->subject(Lang::get('notifications.reset_password.subject'))
+                ->line(Lang::get('notifications.reset_password.intro'))
+                ->action(Lang::get('notifications.reset_password.action'), config('app.frontend_url') . "/password-reset/" . $url . "?email={$notifiable->getEmailForPasswordReset()}")
+                ->line(Lang::get('notifications.reset_password.expiry', [
+                    'count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')
+                ]))
+                ->line(Lang::get('notifications.reset_password.outro'))
+                ->salutation(Lang::get('notifications.salutation'));
         });
     }
 }
